094396136222664f48270e375ae2cfab1f712dd9
delattli <delattli@gmail.com>
2018-11-18 20:45:17 +0100

    Mise à jour de la class ModalWindow

--- src/js/script.js
+++ src/js/script.js
@@ -60,50 +60,56 @@ class ModalWindow {
         this.root = document.createElement("div");
         this.root.classList.add("modal");
         
-        this.block = document.createElement("div");
-        this.block.classList.add("modal__block");
+        let block = document.createElement("div");
+        block.classList.add("modal__block");
         
-        this.title = document.createElement("div");
-        this.title.classList.add("modal__title");
-        this.title.innerHTML = "Modificateur d'&acirc;ge";
+        let title = document.createElement("div");
+        title.classList.add("modal__title");
+        title.innerHTML = "Modificateur d'&acirc;ge";
+        
+        this.select = document.createElement("select");
+        this.select.innerHTML = "<option value=\"50\">50</option><option value=\"49\">49</option><option value=\"48\">48</option>";
+        this.select.innerHTML += "<option value=\"47\">47</option><option value=\"46\">46</option><option value=\"45\">45</option>";
         
         this.content = document.createElement("div");
         this.content.classList.add("modal__line");
-        let content = "Caract&eacute;ristique : <span id=\"charac_name\" class=\"modal__info\">Force</span><br>";
-        content += "Valeur actuelle : <span id=\"old_value\" class=\"modal__info\">50</span><br>";
-        content += "Nouvelle valeur : <select id=\"modal_value\">";
-        content += "<option value=\"50\">50</option><option value=\"49\">49</option><option value=\"48\">48</option>";
-        content += "<option value=\"47\">47</option><option value=\"46\">46</option><option value=\"45\">45</option>";
-        content += "</select>";
+        let content = "Caract&eacute;ristique : <span class=\"modal__info\">Force</span><br>";
+        content += "Valeur actuelle : <span class=\"modal__info\">50</span><br>";
+        content += "Nouvelle valeur : ";
         this.content.innerHTML = content;
+        this.content.appendChild(this.select);
         
-        this.buttons = document.createElement("div");
-        this.buttons.classList.add("modal__buttons");
+        let buttons = document.createElement("div");
+        buttons.classList.add("modal__buttons");
         
         this.button_ok = document.createElement("button");
         this.button_ok.classList.add("modal__buttons_button-first");
         this.button_ok.innerHTML = "OK";
         
-        this.button_cancel = document.createElement("button");
-        this.button_cancel.innerHTML = "ANNULER";
+        let button_cancel = document.createElement("button");
+        button_cancel.innerHTML = "ANNULER";
         
-        this.buttons.appendChild(this.button_ok);
-        this.buttons.appendChild(this.button_cancel);
+        buttons.appendChild(this.button_ok);
+        buttons.appendChild(button_cancel);
         
-        this.block.appendChild(this.title);
-        this.block.appendChild(this.content);
-        this.block.appendChild(this.buttons);
-        this.root.appendChild(this.block);
+        block.appendChild(title);
+        block.appendChild(this.content);
+        block.appendChild(buttons);
+        this.root.appendChild(block);
         
         var objBody = document.getElementsByTagName("body").item(0);
         objBody.appendChild(this.root);
         
         let hide = this.hide.bind(this);
-        this.root.addEventListener("click", hide);
-        this.button_cancel.addEventListener("click", hide);
+        var that = this;
+        window.addEventListener("click", function(e) {
+            if (e.target == that.root) {
+                that.hide(e);
+            }
+        });
+        button_cancel.addEventListener("click", hide);
         this.button_ok.addEventListener("click", function(e) {
             e.preventDefault();
-            e.stopPropagation();
             console.log("Ok");
         });
     }
@@ -113,6 +119,9 @@ class ModalWindow {
     hide(e) {
         e.preventDefault();
         this.root.classList.remove("modal-active");
+    }
+    makeContent() {
+        
     }
 }
 /* Controleur */
